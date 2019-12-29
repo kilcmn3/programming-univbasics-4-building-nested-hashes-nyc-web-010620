b1489cd2d2784c92dbe8bec7547fb6c5cb42c67f
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-29 22:24:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,6 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	puts "!!!!!!!!!!!!"
-	monopoly_with_third_tier[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
+	monopoly_with_third_tier[:railroads][:names][:b_and_o_railroad].merge!("mortgage_value": "bar")
 	puts monopoly_with_third_tier[:railroads][:names][:b_and_o_railroad]
 end
